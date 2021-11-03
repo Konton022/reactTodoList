7dbf81fe6c6b4485d7ffabe254d8478e191e89d4
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-11-03 15:32:02 +0500

    add array of objects

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import { Button, Form, ListGroup } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 import TaskList from "./components/TaskList/index";
-const initState = ["buy fresh bread", "pay for rent", "call boss"];
+//const initState = ["buy fresh bread", "pay for rent", "do homework"];
+const initState = [
+  { id: 1, task: "buy fresh bread", isDone: false },
+  { id: 2, task: "pay for rent", isDone: false },
+  { id: 3, task: "do homework", isDone: false }
+];
 
 export default function App() {
   const [todoList, setToDoList] = useState(initState);
   const [task, setTask] = useState("");
-  console.log(todoList);
+  console.log(todoList, task);
 
   function handleSubmit(event) {
     event.preventDefault();
     setToDoList([...todoList, task]);
+    setTask("");
   }
 
   function handleAddTask(event) {
     setTask(event.target.value);
+  }
+
+  function handleDelTask(id) {
+    console.log(id);
   }
 
   return (
@@ -25,6 +35,7 @@ export default function App() {
         <Form.Group className="mb-3">
           <Form.Label>Add your TASK</Form.Label>
           <Form.Control
+            value={task}
             type="text"
             placeholder="enter your text"
             onChange={handleAddTask}
@@ -34,9 +45,20 @@ export default function App() {
           Submit
         </Button>
       </Form>
-      <ListGroup>
-        {todoList.map((item, index) => (
-          <ListGroup.Item key={index}>{item}</ListGroup.Item>
+      <ListGroup className="mb-3 tasks">
+        {todoList.map(({ id, task, isDone }, index) => (
+          <ListGroup.Item key={index}>
+            <span> {id} </span>
+            <span> {task} </span>
+            <Button
+              variant="secondary"
+              className="btn btn-sm "
+              onClick={(id) => handleDelTask}
+            >
+              {" "}
+              delete{" "}
+            </Button>
+          </ListGroup.Item>
         ))}
       </ListGroup>
     </div>
