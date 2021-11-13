89352e10f6b9fbde1f836fb99925c3ca0e1b09de
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-11-13 18:03:04 +0500

    add reduxjs

--- src/store/task.js
+++ src/store/task.js
@@ -1,8 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
+const initialState = [{ id: 1, task: "hello", isDone: false }];
 
 export const slice = createSlice({
   name: "task",
-  initialState: {
-    data: [{ id: 1, task: "hello", isDone: false }],
+  initialState,
+  reducers: {
+    addTask(state, action) {
+      return state.push({ task: action.payload, isDone: false, id: nanoid(4) });
+    },
+    delTask(state, action) {
+      state;
+    },
+    setDone(state, action) {
+      state;
+    },
   },
 });
+export const { addTask, delTask, setDone } = slice.actions;
+export default slice.reducer;
