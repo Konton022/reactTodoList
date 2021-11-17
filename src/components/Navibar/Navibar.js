14d345214a7a1ba6cd771158fad250d6658d9a8e
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-11-17 14:05:21 +0500

    add nav menu

--- src/components/Navibar/Navibar.js
+++ src/components/Navibar/Navibar.js
@@ -1,16 +1,20 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Button } from "react-bootstrap";
 
 export default function Navibar() {
   return (
-    <Navbar>
+    <Navbar bg="light">
       <Container>
         <Navbar.Brand>Test Page</Navbar.Brand>
         <Navbar.Toggle aria-controls="root-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>Home</Nav>
-          <Nav>Projects</Nav>
-          <Nav>Contact me</Nav>
+          <Nav className="me-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Projects</Nav.Link>
+            <Nav.Link>Contact me</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
+        <Button className="m-2">Log In</Button>
+        <Button>Sing In</Button>
       </Container>
     </Navbar>
   );
