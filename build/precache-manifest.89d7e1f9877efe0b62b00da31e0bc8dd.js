e7ab5eb38c593e34f82cb3cd68de37a1132afd75
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-11-24 23:16:29 +0500

    add build

---
+++ build/precache-manifest.89d7e1f9877efe0b62b00da31e0bc8dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c31100988916a825f8f9",
+    "url": "/static/css/main.7ff96f31.chunk.css"
+  },
+  {
+    "revision": "c31100988916a825f8f9",
+    "url": "/static/js/main.c3110098.chunk.js"
+  },
+  {
+    "revision": "a8323648d145b094edf2",
+    "url": "/static/css/1.c42f7ef1.chunk.css"
+  },
+  {
+    "revision": "a8323648d145b094edf2",
+    "url": "/static/js/1.a8323648.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c959e759868a86c727d97ab1b8268e6e",
+    "url": "/static/media/logo2.c959e759.png"
+  },
+  {
+    "revision": "3946d17ca6418bfadaea93169810271b",
+    "url": "/index.html"
+  }
+];
