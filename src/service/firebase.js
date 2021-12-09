f23e694529aedb924db03ce330551d87c9cfd34f
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-12-09 23:23:38 +0500

    sync

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebase from "firebase/compat/app";
 import "firebase/compat/database";
-
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 import {} from "firebase/database";
 import {} from "firebase/auth";
 
